11319218f9e672a37d673d8fce75db29149d425e
dwalter0 <ds_walters@hotmail.com>
2021-07-28 13:47:54 +1000

    Initial Commit

---
+++ Data/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Scratch.Data
+{
+    public class Config
+    {
+        public string Logging;
+        public string AllowedHosts;
+        public string ClientID;
+        public string ClientSecret;
+        public string RefreshToken;
+    }
+}
